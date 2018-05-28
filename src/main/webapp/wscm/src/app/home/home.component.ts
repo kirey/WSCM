cf540b083df0f61fbda6e480dd194c762fe14557
SERVICES-KIREY\anticm <marinamantic94@gmail.com>
2018-05-28 14:00:21 +0200

    F:  SASS implemented; Removfed app component; Testing home component

--- src/main/webapp/wscm/src/app/home/home.component.ts
+++ src/main/webapp/wscm/src/app/home/home.component.ts
@@ -1,24 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { AppService } from '../shared/services/app.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class HomeComponent implements OnInit {
 
-  constructor(public appService: AppService) { }
+  constructor(public appService: AppService, public sanitizer: DomSanitizer) { }
 
+  style: any;
   header: any;
 
   ngOnInit() {
     this.appService.getContent('home', 'P1')
       .subscribe(
         res => {
-          console.log(res),
-            this.header = res;
+          console.log(res);
+          this.header = res;
+          this.style = this.sanitizer.bypassSecurityTrustStyle(this.header.css);
         },
         err => console.log(err)
       );
