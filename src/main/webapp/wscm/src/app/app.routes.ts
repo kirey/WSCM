521cf1e2d771aef0c73835cd69f4db079edf0994
cvetkovicm <m.cvetkovic@dyntechdoo.com>
2018-06-13 10:48:10 +0200

    F: Languages route entered in sidenav menu

--- src/main/webapp/wscm/src/app/app.routes.ts
+++ src/main/webapp/wscm/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes, RouterModule } from '@angular/router';
 import { ContentComponent } from './content/content.component';
 import { HomeComponent } from './home/home.component';
+import { LanguagesComponent } from './languages/languages.component';
 
 const routes: Routes = [
     { path: 'content', component: ContentComponent },
     { path: 'home', component: HomeComponent },
+    { path: 'languages', component: LanguagesComponent },
 
     { path: '**', component: HomeComponent }
 ];
