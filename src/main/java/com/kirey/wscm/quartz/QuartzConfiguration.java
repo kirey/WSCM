baca91d1d0afa7e8f5734c2c1e5dc5356f50d0d6
Milos Paunovic <m.paunovic@dyntechdoo.com>
2018-06-19 12:28:21 +0200

    B: Method for delete content changed

--- src/main/java/com/kirey/wscm/quartz/QuartzConfiguration.java
+++ src/main/java/com/kirey/wscm/quartz/QuartzConfiguration.java
@@ -1,16 +1,10 @@
 package com.kirey.wscm.quartz;
 
-import static org.quartz.JobBuilder.newJob;
-
 import java.io.IOException;
 
-import org.quartz.Job;
-import org.quartz.JobDetail;
-import org.quartz.JobKey;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 import org.quartz.impl.StdSchedulerFactory;
-import org.quartz.spi.JobFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,9 +12,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.quartz.SpringBeanJobFactory;
-
-import com.kirey.wscm.quartz.jobs.SampleJob1;
-import com.kirey.wscm.quartz.jobs.SampleJob2;
 
 
 @Configuration
@@ -50,20 +41,5 @@ public class QuartzConfiguration {
 
 		return scheduler;
 	}
-
-	@Bean(name = "jobDetail1")
-	public JobDetail jobDetail1() {
-
-		return newJob().ofType(SampleJob1.class).storeDurably().withIdentity(JobKey.jobKey("Qrtz_Job_Detail1"))
-				.withDescription("Invoke Sample Job service...").build();
-	}
-
-	@Bean(name = "jobDetail2")
-	public JobDetail jobDetail2() {
-
-		return newJob().ofType(SampleJob2.class).storeDurably().withIdentity(JobKey.jobKey("Qrtz_Job_Detail2"))
-				.withDescription("Invoke Sample Job service...").build();
-	}
-
 
 }
