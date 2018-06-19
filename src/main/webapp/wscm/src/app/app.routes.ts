4ffb183b42b341a2b3b62a5e2d84a412b458775b
cvetkovicm <m.cvetkovic@dyntechdoo.com>
2018-06-19 13:13:50 +0200

    F: Import Scheduler in sidenav menu

--- src/main/webapp/wscm/src/app/app.routes.ts
+++ src/main/webapp/wscm/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { ContentComponent } from './content/content.component';
 import { HomeComponent } from './home/home.component';
 import { LanguagesComponent } from './languages/languages.component';
 import { LoginComponent } from './login/login.component';
+import { SchedulerComponent } from './scheduler/scheduler.component';
 
 import { AuthGuard } from './shared/guards/auth.guard';
 
@@ -14,8 +15,9 @@ const routes: Routes = [
     pathMatch: 'full'
 },
   { path: 'login', component: LoginComponent },
-  { path: 'content', component: ContentComponent, canActivate: [AuthGuard] },
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'content', component: ContentComponent, canActivate: [AuthGuard] },
+  { path: 'scheduler', component: SchedulerComponent, canActivate: [AuthGuard] },
   { path: 'languages', component: LanguagesComponent },
 
   { path: '**', component: HomeComponent }
