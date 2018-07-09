814b281dd5d40903f49794d82bc34a3102069234
Milos Paunovic <m.paunovic@dyntechdoo.com>
2018-07-09 10:19:39 +0200

    B: added javadoc for some methods

--- src/main/java/com/kirey/wscm/api/restcontrollers/StatisticsController.java
+++ src/main/java/com/kirey/wscm/api/restcontrollers/StatisticsController.java
@@ -14,9 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.kirey.wscm.api.dto.LinksDto;
 import com.kirey.wscm.api.dto.RestResponseDto;
-import com.kirey.wscm.api.dto.StatisticsDto;
 import com.kirey.wscm.data.dao.CategoriesDao;
 import com.kirey.wscm.data.dao.IpAddressDao;
 import com.kirey.wscm.data.dao.IpAddressLinksDao;
@@ -65,7 +63,11 @@ public class StatisticsController {
 	private CategoriesDao categoriesDao;
 	
 	
-	
+	/**
+	 * Method for getting {@link List} of URL with number of request for each URL for given user 
+	 * @param id - of {@link WscmUserAccounts}
+	 * @return ResponseEntity containing the HashMap with user details and list of URL 
+	 */
 	@RequestMapping(value = "/link/user/{id}", method = RequestMethod.GET)
 	public ResponseEntity<RestResponseDto> linksStatisticsByUser(@PathVariable Integer id) {
 		Map<String, Object> response = new HashMap<>();
@@ -93,6 +95,11 @@ public class StatisticsController {
 		return new ResponseEntity<RestResponseDto>(new RestResponseDto(response, HttpStatus.OK.value()), HttpStatus.OK);
 	}
 	
+	/**
+	 * Method for getting {@link List} of URL with number of request for each URL for given ip address 
+	 * @param address - ip address
+	 * @return ResponseEntity containing the HashMap with list of URL 
+	 */
 	@RequestMapping(value = "/link/ip", method = RequestMethod.GET)
 	public ResponseEntity<RestResponseDto> linksStatisticsByUser(@RequestParam String address) {
 		Map<String, Object> response = new HashMap<>();
@@ -114,6 +121,11 @@ public class StatisticsController {
 		return new ResponseEntity<RestResponseDto>(new RestResponseDto(response, HttpStatus.OK.value()), HttpStatus.OK);
 	}
 	
+	/**
+	 * Method for getting {@link List} of {@link Categories} to which is accessed by given user 
+	 * @param id
+	 * @return ResponseEntity containing the HashMap with user details and list of categories with number of request
+	 */
 	@RequestMapping(value = "/categoryNo/user/{id}", method = RequestMethod.GET)
 	public ResponseEntity<RestResponseDto> categoryNoByUser(@PathVariable Integer id) {
 	Map<String, Object> response = new HashMap<>();
@@ -152,6 +164,11 @@ public class StatisticsController {
 		return new ResponseEntity<RestResponseDto>(new RestResponseDto(response, HttpStatus.OK.value()), HttpStatus.OK);
 	}
 	
+	/**
+	 * Method for getting {@link List} of {@link Categories} to which is accessed by given ip address
+	 * @param address
+	 * @return ResponseEntity containing the HashMap with list of categories with number of request
+	 */
 	@RequestMapping(value = "/categoryNo/ip", method = RequestMethod.GET)
 	public ResponseEntity<RestResponseDto> categoryNoIpAddress(@RequestParam String address) {
 
@@ -189,6 +206,10 @@ public class StatisticsController {
 		return new ResponseEntity<RestResponseDto>(new RestResponseDto(response, HttpStatus.OK.value()), HttpStatus.OK);
 	}
 	
+	/**
+	 * Method for getting number of requests for each category 
+	 * @return ResponseEntity containing the List of HashMap with list of categories with number of request
+	 */
 	@RequestMapping(value = "/categoryNo", method = RequestMethod.GET)
 	public ResponseEntity<RestResponseDto> categoryNo() {
 //		Map<String, Object> responseMap = new HashMap<>();
@@ -215,6 +236,11 @@ public class StatisticsController {
 		return new ResponseEntity<RestResponseDto>(new RestResponseDto(responseList, HttpStatus.OK.value()), HttpStatus.OK);
 	}
 	
+	/**
+	 * Method for getting {@link List} of URL with number of requests by given category
+	 * @param id - of {@link Categories}
+	 * @return ResponseEntity containing the HashMap with category details and list of URL with number of request
+	 */
 	@RequestMapping(value = "/categoryNo/{id}", method = RequestMethod.GET)
 	public ResponseEntity<RestResponseDto> categoryNoByCategory(@PathVariable Integer id) {
 		Map<String, Object> response = new HashMap<>();
