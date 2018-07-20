c727b50dcf3287ee6179bb7ff363fc331c2aee73
SERVICES-KIREY\anticm <marinamantic94@gmail.com>
2018-07-20 11:08:28 +0200

    F: Minor changes

--- src/main/webapp/wscm/src/app/shared/dialogs/add-job-dialog/add-job-dialog.service.ts
+++ src/main/webapp/wscm/src/app/shared/dialogs/add-job-dialog/add-job-dialog.service.ts
@@ -9,54 +9,23 @@ export class AddJobService {
   constructor(private _http: HttpClient) { }
 
   baseUrl = '/wscm/rest/scheduler/';
-<<<<<<< Updated upstream
-// Get Alll Categories
-getAllCategories() {
-  return this._http.get(this.baseUrl + '/' + 'categories').pipe(map(res => res));
-}
-// Get All Job Types
-getAllJobs(): Observable<any> {
-  return this._http.get(this.baseUrl + '/' + 'jobTypes');
-}
-// Class Loading - Get All Classes
-getAllClasses(): Observable<any> {
-  return this._http.get(this.baseUrl + '/' + 'classes');
-}
-// Get All Notifications - multiselect
-getAllNotification(): Observable<any> {
-  return this._http.get(this.baseUrl + '/' + 'notifications');
-}
-  // addJobs(jobs): Observable<any> {
-  //   return this._http.post(this.baseUrl + 'events', jobs);
-  // }
-  // getJobs(): Observable<any> {
-  //   return this._http.get(this.baseUrl + 'events');
-  // }
-  // editJobs(obj): Observable<any> {
-  //   return this._http.put(this.baseUrl + 'events', obj);
-  // }
-  // getJobs(): Observable<any> {
-  //   return this._http.get(this.baseUrl + 'events');
-  // }
-=======
   // Get Alll Categories
   getAllCategories() {
-    return this._http.get(this.baseUrl + '/' + 'categories').pipe(map(res => res));
+    return this._http.get(this.baseUrl + 'categories').pipe(map(res => res));
   }
   // Get All Job Types
   getAllJobs(): Observable<any> {
-    return this._http.get(this.baseUrl + '/' + 'jobTypes');
+    return this._http.get(this.baseUrl + 'jobTypes');
   }
   // Class Loading - Get All Classes
   getAllClasses(): Observable<any> {
-    return this._http.get(this.baseUrl + '/' + 'classes');
+    return this._http.get(this.baseUrl + 'classes');
   }
 
   getAllNotification(): Observable<any> {
-    return this._http.get(this.baseUrl + '/' + 'notifications');
+    return this._http.get(this.baseUrl + 'notifications');
   }
   addJob(obj): Observable<any> {
-    return this._http.post(this.baseUrl + 'add/Job', obj);
+    return this._http.post(this.baseUrl + 'addJob', obj);
   }
->>>>>>> Stashed changes
 }
